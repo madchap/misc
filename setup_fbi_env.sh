a90e7b5ed3a7b1ded31356caf087e0d2f66ab343
fblaise <fred.blaise@kudelskisecurity.com>
2016-10-18 13:21:15 +0200

    helper script to get zsh and vim on new host

---
+++ setup_fbi_env.sh
@@ -0,0 +1,38 @@
+sudo apt-get -y install zsh zsh-syntax-highlighting gitlab-shell git curl vim terminator powerline xfonts-terminus
+
+if [ $(getent passwd $(whoami) | cut -d: -f7) = "/bin/bash" ]; then
+	echo "Changing shell to zsh.. please enter your password."
+	chsh -s $(which zsh)
+fi
+
+if [ ! -d ~/gitrepos ]; then 
+	mkdir ~/gitrepos 
+fi
+
+# zsh
+if [ -d  ~/.oh-my-zsh ]; then
+	cd .oh-my-zsh
+	git pull
+else
+	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+	cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git pull
+else
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+# vim
+cd ~/gitrepos
+if [ ! -d vim-wombat256mod ]; then
+	git clone https://github.com/michalbachowski/vim-wombat256mod.git
+	sudo cp vim-wombat256mod/colors/wombat256mod.vim /usr/share/vim/vim74/colors
+fi
+
+# get custom files from git
+curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/.vimrc > ~/.vimrc
+curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/.zshrc > ~/.zshrc
+curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/zsh_autosuggestions_config.zsh > ~/.oh-my-zsh/custom/config.zsh
+
