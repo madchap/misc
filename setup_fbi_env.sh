04651bf25d90337a841c709dbf6577271e6af261
Fred Blaise <fred.blaise@gmail.com>
2016-10-22 21:07:28 +0200

    if'ed specific linux stuff

--- setup_fbi_env.sh
+++ setup_fbi_env.sh
@@ -1,8 +1,10 @@
-sudo apt-get -y install zsh zsh-syntax-highlighting gitlab-shell git curl vim terminator powerline xfonts-terminus python-pip jq tmux
+if [ uname -v |grep -q Ubuntu ]; then
+	sudo apt-get -y install zsh zsh-syntax-highlighting gitlab-shell git curl vim terminator powerline xfonts-terminus python-pip jq tmux
 
-if [ $(getent passwd $(whoami) | cut -d: -f7) = "/bin/bash" ]; then
-	echo "Changing shell to zsh.. please enter your password."
-	chsh -s $(which zsh)
+	if [ $(getent passwd $(whoami) | cut -d: -f7) = "/bin/bash" ]; then
+		echo "Changing shell to zsh.. please enter your password."
+		chsh -s $(which zsh)
+	fi
 fi
 
 if [ ! -d ~/gitrepos ]; then 
