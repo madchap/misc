9a9e88bbb80828e42ae7f1cad947fe4952232f35
Fred Blaise <fred.blaise@gmail.com>
2016-11-13 19:40:14 +0100

    changes to sync script

--- sync_remote_tmux_and_vi.sh
+++ sync_remote_tmux_and_vi.sh
@@ -5,12 +5,12 @@ RSYNC="$(which rsync) -rtuL"
 
 # sync tmux config and plugins from local
 echo "Sync'ing tmux config and plugins..."
-$RSYNC ~/.tmux/plugins $1:~ 
+$RSYNC ~/.tmux/plugins $1:~/.tmux/
 $RSYNC ~/.tmux-server.conf $1:~/.tmux.conf
 
 echo "Sync'ing vim config and plugins..."
 # sync vim config
-$RSYNC ~/.vim $1:~
+$RSYNC ~/.vim $1:~/.vim/
 $RSYNC ~/.vimrc $1:~
 
 TMUX_SESSION_NAME="fbi-mux"
