777f7f5108f2e2e91aa81a64b95869081ac58f26
Fred Blaise <fred.blaise@gmail.com>
2016-11-13 17:31:06 +0100

    Adding alias to get added on setup

--- setup_fbi_env.sh
+++ setup_fbi_env.sh
@@ -48,6 +48,7 @@ curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/m
 curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/.tmux.conf > ~/.tmux.conf
 curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/.tmux-macos.conf > ~/.tmux-macos.conf
 curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/.sshrc > ~/.ssh/rc
+curl -H 'Cache-Control: no-cache' -s https://raw.githubusercontent.com/madchap/misc/master/.alias > ~/.alias
 
 # pip
 # pip install --upgrade pip
