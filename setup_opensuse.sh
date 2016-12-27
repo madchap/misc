6d3d94670111f643e3384a07387c97ca6e30ce2c
Fred Blaise <fred.blaise@gmail.com>
2016-12-27 22:49:17 +0100

    adding specific opensuse stuff. will refactor later

---
+++ setup_opensuse.sh
@@ -0,0 +1,3 @@
+sudo zypper ar http://download.videolan.org/pub/vlc/SuSE/Tumbleweed VLC
+sudo zypper ar http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ Packman
+sudo zypper --non-interactive install vlc-codecs
