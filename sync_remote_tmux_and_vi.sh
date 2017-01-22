99ac264960d09a06857d97bab861bed9630c876e
Fred Blaise <fred.blaise@gmail.com>
2017-01-22 18:50:31 +0100

    added force option to rsync only when wanted

--- sync_remote_tmux_and_vi.sh
+++ sync_remote_tmux_and_vi.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
-
 [[ -z $1 ]] && echo "Need a host to connect to." && exit -1
+
+while getopts "s:fh" opt; do
+	case $opt in 
+		"h")
+			echo "Usage $0 -s MYHOST [-f] [-h]"
+			echo "	-f : force rsync of vim and tmux plugins"
+			echo "	-h : help"
+			exit 0
+			;;
+		"f")
+			FORCE=1
+			;;
+		"s")
+			MYHOST=${OPTARG}
+			;;
+		"*")
+			echo "Unknown option."
+			exit 2
+			;;
+	esac
+done
+
 RSYNC="$(which rsync) -rtuL"
 
-# sync tmux config and plugins from local
-echo "Sync'ing tmux config and plugins..."
-$RSYNC --exclude 'tmux_resurrect_*' ~/.tmux/ $1:~/.tmux/
-$RSYNC ~/.tmux-server.conf $1:~/.tmux.conf
+if [ $FORCE ]; then
+	# sync tmux config and plugins from local
+	echo "Sync'ing tmux config and plugins..."
+	$RSYNC --exclude 'tmux_resurrect_*' ~/.tmux/ $MYHOST:~/.tmux/
+	$RSYNC ~/.tmux-server.conf $MYHOST:~/.tmux.conf
 
-echo "Sync'ing vim config and plugins..."
-# sync vim config
-$RSYNC ~/.vim/ $1:~/.vim/
-$RSYNC ~/.vimrc-server $1:~/.vimrc
+	echo "Sync'ing vim config and plugins..."
+	# sync vim config
+	$RSYNC ~/.vim/ $MYHOST:~/.vim/
+	$RSYNC ~/.vimrc-server $MYHOST:~/.vimrc
+fi
 
 TMUX_SESSION_NAME="fbi-mux"
-ssh -t $1 "hash tmux 2>/dev/null || sudo yum -y install tmux; tmux attach -t $TMUX_SESSION_NAME || tmux new -s $TMUX_SESSION_NAME"
+ssh -t $MYHOST "hash tmux 2>/dev/null || sudo yum -y install tmux; tmux attach -t $TMUX_SESSION_NAME || tmux new -s $TMUX_SESSION_NAME"
