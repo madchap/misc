fd2ce79206a9ae8b563a9a773962769a6b0b2a36
Fred Blaise <fred.blaise@gmail.com>
2017-02-12 16:34:59 +0100

    minor cleanup

--- scripts/scan_nfs_to_evernote.sh
+++ scripts/scan_nfs_to_evernote.sh
@@ -10,6 +10,8 @@ logfile=/home/fblaise/evernote_upload.log
 # in seconds
 scan_max_value=315
 scan_min_value=15
+nfs_server="192.168.10.25"
+nfs_export_point="/Scans"
 scan_point="/home/fblaise/nfs/scans"
 email_to="fred.blaise@gmail.com"
 
@@ -20,7 +22,6 @@ log_it() {
 }
 
 check_nfs_mount() {
-	log_it "Checking NFS mount."
 	count=1
 	nfs_ok=1
 
@@ -28,9 +29,8 @@ check_nfs_mount() {
 		local fs=$(stat -f -L -c %T ${scan_point})
 		if [ "$fs" != "nfs" ]; then
 			log_it "NFS not mounted. Attempting now (Attempt $count)."
-			sudo mount -t nfs 192.168.10.25:/Scans /home/fblaise/nfs/scans
+			sudo mount -t nfs ${nfs_server}:{$nfs_export_point} ${scan_point}
 		else
-			log_it "NFS mount OK."
 			nfs_ok=0
 		fi
 		((count++))
@@ -44,10 +44,6 @@ send_email() {
 	echo "$body" | mail -s "$subject" "$email_to"
 }
 
-log_it "Starting." 
-log_it "Upper and lower time limits on file to search: $scan_max_value $scan_min_value"
-log_it "Scan point: $scan_point"
-
 check_nfs_mount
 
 # Look for file
@@ -60,10 +56,10 @@ for file in ${files}; do
 	# upload via geeknote
 	$geeknote create --title "${file##*/}" --resource "$file" --content "" --tag "${tag_triage}" --notebook ${notebook} >> $logfile
 	if [ $? -ne 0 ]; then
-		log_it "Error $? when trying to upload file."
+		log_it "Error $? when trying to upload $file."
 		send_email "Geeknote scan error" "$(tail -n100 $logfile)"
 	else
-		log_it "Ok, file uploaded."
+		log_it "Ok, $file uploaded."
 		send_email "Evernote: New files to be triaged" "You've got new files that need to be triaged."
 	fi
 done
