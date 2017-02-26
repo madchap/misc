5a934c67f6d873ad181419678fbcea0811af6c3c
Fred Blaise <fred.blaise@kudelskisecurity.com>
2017-02-26 16:55:57 +0100

    btrfs move .cache out to subvol

--- snapper/snapper_home.sh
+++ snapper/snapper_home.sh
@@ -1,3 +1,13 @@
+echo "Moving ~/.cache to its own subvol"
+sudo btrfs sub create /home/fblaise/.cache-sub
+sudo chown fblaise: /home/fblaise/.cache-sub
+rsync -a ~/.cache/* /home/fblaise/.cache-sub/
+mv /home/fblaise/.cache /home/fblaise/.cache-old
+mv /home/fblaise/.cache-sub /home/fblaise/.cache
+sudo btrfs sub list /home
+
+echo
+echo "Setting up snapper for /home"
 sudo snapper -c home create-config /home
 sudo snapper -c home set-config "NUMBER_CLEANUP=no"
 sudo snapper -c home set-config "NUMBER_LIMIT=0"
