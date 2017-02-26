018793c8b311134566d046252045382331bc423b
Fred Blaise <fred.blaise@gmail.com>
2017-02-26 14:56:03 +0100

    snapper small changes

--- snapper/snapper_home.sh
+++ snapper/snapper_home.sh
@@ -4,9 +4,10 @@ sudo snapper -c home set-config "NUMBER_LIMIT=0"
 sudo snapper -c home set-config "NUMBER_LIMIT_IMPORTANT=0"
 sudo snapper -c home set-config "TIMELINE_LIMIT_DAILY=7"
 sudo snapper -c home set-config "TIMELINE_LIMIT_HOURLY=24"
-sudo snapper -c home set-config "TIMELINE_LIMIT_WEEKLY=2"
+sudo snapper -c home set-config "TIMELINE_LIMIT_WEEKLY=0"
 sudo snapper -c home set-config "TIMELINE_LIMIT_MONTHLY=0"
 sudo snapper -c home set-config "TIMELINE_LIMIT_YEARLY=0"
 sudo snapper -c home set-config "SPACE_LIMIT=0.2"
+sudo snapper -c home set-config "QGROUP=1/0"
 sudo snapper -c home get-config
 sudo snapper -c home list
