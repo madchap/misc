1f7279dca111f9624bca1db673541576237f1050
Fred Blaise <fred.blaise@gmail.com>
2017-09-03 10:45:52 +0200

    reduce to 3 days snaps for home

--- snapper/snapper_home.sh
+++ snapper/snapper_home.sh
@@ -13,7 +13,7 @@ sudo snapper -c home create-config /home
 sudo snapper -c home set-config "NUMBER_CLEANUP=no"
 sudo snapper -c home set-config "NUMBER_LIMIT=0"
 sudo snapper -c home set-config "NUMBER_LIMIT_IMPORTANT=0"
-sudo snapper -c home set-config "TIMELINE_LIMIT_DAILY=7"
+sudo snapper -c home set-config "TIMELINE_LIMIT_DAILY=3"
 sudo snapper -c home set-config "TIMELINE_LIMIT_HOURLY=24"
 sudo snapper -c home set-config "TIMELINE_LIMIT_WEEKLY=0"
 sudo snapper -c home set-config "TIMELINE_LIMIT_MONTHLY=0"
