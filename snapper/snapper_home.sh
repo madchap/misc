46c5996bb49bfd906e02a651588cf6ab86e1d80c
Fred <fred.blaise@gmail.com>
2018-01-13 16:20:13 +0100

    fixed snapper failure in re-creating home config

--- snapper/snapper_home.sh
+++ snapper/snapper_home.sh
@@ -21,18 +21,23 @@ if [ $? != 0 ]; then
 	sudo btrfs sub list /home
 fi
 
-echo
-echo "Setting up snapper for /home"
-sudo snapper -c home create-config /home
-sudo snapper -c home set-config "NUMBER_CLEANUP=no"
-sudo snapper -c home set-config "NUMBER_LIMIT=0"
-sudo snapper -c home set-config "NUMBER_LIMIT_IMPORTANT=0"
-sudo snapper -c home set-config "TIMELINE_LIMIT_DAILY=3"
-sudo snapper -c home set-config "TIMELINE_LIMIT_HOURLY=24"
-sudo snapper -c home set-config "TIMELINE_LIMIT_WEEKLY=0"
-sudo snapper -c home set-config "TIMELINE_LIMIT_MONTHLY=0"
-sudo snapper -c home set-config "TIMELINE_LIMIT_YEARLY=0"
-sudo snapper -c home set-config "SPACE_LIMIT=0.2"
-sudo snapper -c home set-config "QGROUP=1/0"
-sudo snapper -c home get-config
-sudo snapper -c home list
+sudo snapper list-configs |grep -q home
+if [ $? != 0 ]; then
+	echo
+	echo "Setting up snapper for /home"
+	sudo snapper -c home create-config /home
+	sudo snapper -c home set-config "NUMBER_CLEANUP=no"
+	sudo snapper -c home set-config "NUMBER_LIMIT=0"
+	sudo snapper -c home set-config "NUMBER_LIMIT_IMPORTANT=0"
+	sudo snapper -c home set-config "TIMELINE_LIMIT_DAILY=3"
+	sudo snapper -c home set-config "TIMELINE_LIMIT_HOURLY=24"
+	sudo snapper -c home set-config "TIMELINE_LIMIT_WEEKLY=0"
+	sudo snapper -c home set-config "TIMELINE_LIMIT_MONTHLY=0"
+	sudo snapper -c home set-config "TIMELINE_LIMIT_YEARLY=0"
+	sudo snapper -c home set-config "SPACE_LIMIT=0.2"
+	sudo snapper -c home set-config "QGROUP=1/0"
+	sudo snapper -c home get-config
+	sudo snapper -c home list
+else
+	echo "snapper configuration already exists for home. Not attempting."
+fi
