500a5297247caa7cd8095ca1cf161e5f587df56f
Fred Blaise <fred.blaise@gmail.com>
2018-04-03 13:25:02 +0200

    update to owncloud letsecrypt renewal

--- owncloud/renew_certs.sh
+++ owncloud/renew_certs.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-set -eux
 
+set -eux
+ 
+cleanup() {
+        # kill http server
+        kill $pypid
+	# _owncloud start
+}
+
+_owncloud() {
+	if [ "$1" == "stop" ]; then
+		docker stop owncloud
+	elif [ "$1" == "start" ]; then
+		docker start owncloud
+	fi
+}
+
+trap cleanup INT TERM EXIT
+ 
+wwwdir=/var/www/html
+
+# _owncloud stop
+ 
 # Start python simple server on port 80 for letsencrypt check
-cd /var/www/html
+[[ -d $wwwdir ]] || mkdir -p $wwwdir
+cd $wwwdir
 python -m SimpleHTTPServer 80 &
 pypid=$!
-
+ 
+sleep 5
+ 
 # invoking certbot
-# full
-# certbot certonly --webroot -w /var/www/html -d domain_name
+# full -- not done automatically. Do the initial certs at least once.
+# certbot certonly --webroot -w $wwwdir -d logstash.cyberfusion.center
 # renew
-certbot renew --post-hook "/root/certbot/cp_owncloud_certs.sh" 
+certbot renew --post-hook "/root/certbot/cp_owncloud_certs.sh"
 
-# kill http server
-kill $pypid
+# _owncloud start
