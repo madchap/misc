29924a8155abbaadb21152f67c8245c915f57d2d
Fred Blaise <fred.blaise@gmail.com>
2018-07-06 22:47:56 +0200

    - changed folder - nginx rp restart - owncloud apache reload

--- certbot/cp_owncloud_certs.sh
+++ certbot/cp_owncloud_certs.sh
@@ -1,4 +1,10 @@
-cp -f /etc/letsencrypt/live/q.darthgibus.net/fullchain.pem ssl-cert.crt
-cp -f /etc/letsencrypt/live/q.darthgibus.net/privkey.pem ssl-cert.key
+# weechat relay
+cat /etc/letsencrypt/live/q.darthgibus.net/privkey.pem /etc/letsencrypt/live/q.darthgibus.net/fullchain.pem > /home/fblaise/.weechat/ssl/relay.pem
+# fifo into weechat
+su - fblaise -c "echo '*/relay sslcertkey'" > /home/fblaise/.weechat/weechat_fifo
+su - fblaise -c "echo '*/reload relay'" > /home/fblaise/.weechat/weechat_fifo
 
+# owncloud.. 
+cp -f /etc/letsencrypt/live/q.darthgibus.net/fullchain.pem /mnt/pydio/shares/owncloud/certs/ssl-cert.crt
+cp -f /etc/letsencrypt/live/q.darthgibus.net/privkey.pem /mnt/pydio/shares/owncloud/certs/ssl-cert.key
 docker exec owncloud /usr/sbin/apachectl restart
--- certbot/renew_certs.sh
+++ certbot/renew_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eux
+set -ex
  
 cleanup() {
         # kill http server
@@ -8,11 +8,14 @@ cleanup() {
 	# _owncloud start
 }
 
-_owncloud() {
+_restart_container() {
+	# $1 action
+	# $2 container name
+
 	if [ "$1" == "stop" ]; then
-		docker stop owncloud
+		docker stop $2
 	elif [ "$1" == "start" ]; then
-		docker start owncloud
+		docker start $2
 	fi
 }
 
@@ -20,7 +23,7 @@ trap cleanup INT TERM EXIT
  
 wwwdir=/var/www/html
 
-# _owncloud stop
+_restart_container stop nginx
  
 # Start python simple server on port 80 for letsencrypt check
 [[ -d $wwwdir ]] || mkdir -p $wwwdir
@@ -34,6 +37,6 @@ sleep 5
 # full -- not done automatically. Do the initial certs at least once.
 # certbot certonly --webroot -w $wwwdir -d logstash.cyberfusion.center
 # renew
-certbot renew --post-hook "/root/certbot/cp_owncloud_certs.sh"
+/usr/bin/certbot renew --post-hook "/root/certbot/cp_owncloud_certs.sh"
 
-# _owncloud start
+_restart_container start nginx
