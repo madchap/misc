56281cab4634643ccb57af1922f330d6c7c884c5
Fred Blaise <fred.blaise@protonmail.com>
2020-03-07 17:45:16 +0100

    certs copy update

--- certbot/cp_owncloud_certs.sh
+++ certbot/cp_owncloud_certs.sh
@@ -5,6 +5,10 @@ su - fblaise -c "echo '*/relay sslcertkey'" > /home/fblaise/.weechat/weechat_fif
 su - fblaise -c "echo '*/reload relay'" > /home/fblaise/.weechat/weechat_fifo
 
 # owncloud.. 
-cp -f /etc/letsencrypt/live/q.darthgibus.net/fullchain.pem /mnt/pydio/shares/owncloud/certs/ssl-cert.crt
-cp -f /etc/letsencrypt/live/q.darthgibus.net/privkey.pem /mnt/pydio/shares/owncloud/certs/ssl-cert.key
-docker exec owncloud /usr/sbin/apachectl restart
+# cp -f /etc/letsencrypt/live/q.darthgibus.net/fullchain.pem /mnt/pydio/shares/owncloud/certs/ssl-cert.crt
+# cp -f /etc/letsencrypt/live/q.darthgibus.net/privkey.pem /mnt/pydio/shares/owncloud/certs/ssl-cert.key
+# docker exec owncloud /usr/sbin/apachectl restart
+
+# vault
+cp -f /etc/letsencrypt/live/vault.darthgibus.net/fullchain.pem /var/lib/docker/volumes/vault_file/_data/ssl-cert.crt
+cp -f /etc/letsencrypt/live/vault.darthgibus.net/privkey.pem /var/lib/docker/volumes/vault_file/_data/ssl-cert.key
