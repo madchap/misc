00fb69e795822e66cd9879beddc9f4a019cfca08
Fred Blaise <fred.blaise@protonmail.com>
2020-06-23 10:17:03 +0200

    testing xrandr with proper order file

--- scripts/udev_plug_dock.sh
+++ scripts/udev_plug_dock.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # run through 'at' coming from udev
+set -x
 
 # wait for the system to stabilize
 sleep 3
@@ -8,12 +9,12 @@ export XAUTHORITY=/home/fblaise/.Xauthority
 export DISPLAY=:0
 
 XRANDRNOW_FILE=/tmp/xrandr-current.txt
-REFERENCE_XRANDR=/home/fblaise/wrong_4k_order.txt
+REFERENCE_XRANDR=/home/fblaise/proper_4k_order.txt
 
 xrandr --prop | grep -A2 EDID > "$XRANDRNOW_FILE"
 # "wrong" file currently has 4k monitors the wrong way
 if [[ -e "$REFERENCE_XRANDR" ]]; then
-    if diff -q "$XRANDRNOW_FILE" "$XRANDRNOW_FILE"; then
+    if ! diff -q "$XRANDRNOW_FILE" "$XRANDRNOW_FILE"; then
         if [[ $(wc -l "$XRANDRNOW_FILE" |cut -d' ' -f1) -lt 8 ]]; then
             echo "DP2 is not showing as connected..."
             /home/fblaise/.i3/scripts/xrandr.sh eDP1 DP1
