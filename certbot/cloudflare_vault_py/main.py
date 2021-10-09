f45eaaf24fe809d97997b573f9d2a8fa5678a605
Fred Blaise <fred.blaise@protonmail.com>
2021-10-09 16:28:03 +0200

    signal

--- certbot/cloudflare_vault_py/main.py
+++ certbot/cloudflare_vault_py/main.py
@@ -10,6 +10,8 @@ import docker_helper
 import webserver_helper
 import email_helper
 import time
+import os
+import signal
 
 
 def get_cf_token_from_vault():
@@ -60,7 +62,7 @@ def flip_dns_proxy(switch=True):
     try:
         dns_records = cf.zones.dns_records.get(zone_id)
         for dns in dns_records:
-            records_to_process = ["pw", "q", "vault"]
+            records_to_process = ["pw", "q", "vault", "n"]
             if dns['name'].split('.')[0] in records_to_process:
                 print(json.dumps(dns, indent=4, sort_keys=True))
                 print("Switching proxy state to {}".format(switch))
@@ -71,18 +73,27 @@ def flip_dns_proxy(switch=True):
 
     except CloudFlare.exceptions.CloudFlareAPIError as e:
         exit("DNS records call failed {}".format(e))
+        email_helper.send_email("fred.blaise@gmail.com", "Certs renewal - Cloudflare API call failed.")
 
 
+def timeout_handler(num, stack):
+    print("We've been waiting for too long. Let's kill process and restart nginx")
+    webserver_helper.down()
+    docker_helper.start_container("nginx-rp")
+    flip_dns_proxy(True)
+    sys.exit(2)
+
+
+signal.signal(signal.SIGALRM, timeout_handler)
+signal.alarm(360)
 if __name__ == "__main__":
     cftoken = get_cf_token_from_vault()
     cf = CloudFlare.CloudFlare(debug=False, token=cftoken)
     flip_dns_proxy(False)
-
     webserver_helper.up()
     time.sleep(1)
     docker_helper.stop_container("nginx-rp")
     exec_certbot()
     webserver_helper.down()
     docker_helper.start_container("nginx-rp")
-    
     flip_dns_proxy(True)
